a46d1f3eec77fa8084c8db87e2209c7aa7ab0de1
polyooe <lhb293425@gmail.com>
2023-04-30 08:00:12 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 
-# 克隆 momoka 代码
-git clone https://github.com/lens-protocol/momoka.git
+# 定义 Momoka 镜像名称和版本
+MOMOKA_IMAGE="lensprotocol/momoka"
+MOMOKA_VERSION="latest"
 
-# 构建 Docker 镜像
-docker build -t momoka .
+# 定义默认端口和备用端口
+DEFAULT_PORT="3000"
+BACKUP_PORT="3001"
 
-# 创建 Docker 网络
-docker network create momoka-network
+# 检查默认端口是否已被占用，如果被占用则使用备用端口
+if [[ $(netstat -nl | grep ":$DEFAULT_PORT") ]]; then
+  echo "端口 $DEFAULT_PORT 已被占用，将使用备用端口 $BACKUP_PORT"
+  PORT=$BACKUP_PORT
+else
+  PORT=$DEFAULT_PORT
+fi
 
-# 运行 PostgreSQL 容器
-docker run -d --name momoka-postgres --network momoka-network -e POSTGRES_PASSWORD=password -e POSTGRES_DB=momoka -v momoka-postgres-data:/var/lib/postgresql/data postgres:12
+# 构建 Momoka 镜像
+docker build -t $MOMOKA_IMAGE:$MOMOKA_VERSION .
 
-# 运行 Redis 容器 
-docker run -d --name momoka-redis --network momoka-network redis:6
+# 运行 Momoka 容器
+docker run -d -p $PORT:3000 $MOMOKA_IMAGE:$MOMOKA_VERSION
 
-# 运行 momoka 容器
-docker run -d --name momoka \
-  --network momoka-network \
-  -e DATABASE_URL=postgres://postgres:password@momoka-postgres:5432/momoka \
-  -e REDIS_URL=redis://momoka-redis:6379 \
-  -v momoka-data:/app/public/uploads \
-  -p 3008:3008 \
-  momoka
+# 显示 Momoka 容器信息
+CONTAINER_ID=$(docker ps -qf "ancestor=$MOMOKA_IMAGE:$MOMOKA_VERSION")
+CONTAINER_NAME=$(docker inspect --format '{{.Name}}' $CONTAINER_ID | sed 's/\///')
+CONTAINER_IP=$(docker inspect --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CONTAINER_ID)
 
-# momoka 应用部署完成
-echo "momoka deployed!"
-
+echo "Momoka 容器已经启动，端口号为 $PORT"
+echo "网页访问地址: http://localhost:$PORT/"
+echo "配置修改地址: http://localhost:$PORT/settings"
+echo "容器名称: $CONTAINER_NAME"
+echo "容器IP地址: $CONTAINER_IP"
